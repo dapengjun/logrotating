0dd3d23533b1a093ae4603a4659fbefba630dcda
hongfazhizun <hongfazhizun@gmail.com>
2017-10-27 22:30:43 +0800

    add all log level functions

--- log.go
+++ log.go
@@ -39,7 +39,7 @@ type Logger struct {
 	mu    sync.Mutex
 	out   io.Writer
 	file  string
-	size  int // 0代表无限大，文件大于字节数时，将文件重命名
+	size  int // 0 disable rotating，rotating while file size is large than size
 	flag  int
 	level int
 }
@@ -54,8 +54,114 @@ func SetLogger(logger *Logger) {
 	std = logger
 }
 
+func SetFlag(flag int) {
+	std.mu.Lock()
+	defer std.mu.Unlock()
+	std.flag = flag
+}
+
+func SetFile(file string, size int) {
+	std.mu.Lock()
+	defer std.mu.Unlock()
+	switch f := std.out.(type) {
+	case *os.File:
+		if f == os.Stderr {
+
+		} else if f == os.Stdout {
+
+		} else {
+			f.Close()
+			std.out = nil
+			t := time.Now()
+			timestamp := t.Unix()
+			newFileName := fmt.Sprintf("%s.%d", std.file, timestamp)
+			os.Rename(std.file, newFileName)
+		}
+	}
+	std.file = file
+	std.size = size
+	f, _ := os.Open(file)
+	std.out = f
+}
+
+func SetStderr() {
+	std.mu.Lock()
+	defer std.mu.Unlock()
+	switch f := std.out.(type) {
+	case *os.File:
+		if f == os.Stderr {
+
+		} else if f == os.Stdout {
+
+		} else {
+			f.Close()
+			std.out = nil
+			t := time.Now()
+			timestamp := t.Unix()
+			newFileName := fmt.Sprintf("%s.%d", std.file, timestamp)
+			os.Rename(std.file, newFileName)
+		}
+	}
+	std.file = ""
+	std.size = 0
+	std.out = os.Stderr
+}
+
+func SetStdout() {
+	std.mu.Lock()
+	defer std.mu.Unlock()
+	switch f := std.out.(type) {
+	case *os.File:
+		if f == os.Stderr {
+
+		} else if f == os.Stdout {
+
+		} else {
+			f.Close()
+			std.out = nil
+			t := time.Now()
+			timestamp := t.Unix()
+			newFileName := fmt.Sprintf("%s.%d", std.file, timestamp)
+			os.Rename(std.file, newFileName)
+		}
+	}
+	std.file = ""
+	std.size = 0
+	std.out = os.Stdout
+}
+
 func SetLogLevel(level int) {
 	std.SetLogLevel(level)
+}
+
+func Panic(args ...interface{}) {
+	text := std.print(2, LOG_LEVEL_PANIC, args...)
+	panic(text)
+}
+
+func Panicln(args ...interface{}) {
+	text := std.println(2, LOG_LEVEL_PANIC, args...)
+	panic(text)
+}
+
+func Panicf(format string, args ...interface{}) {
+	text := std.printf(2, LOG_LEVEL_PANIC, format, args...)
+	panic(text)
+}
+
+func Fatal(args ...interface{}) {
+	std.print(2, LOG_LEVEL_FATAL, args...)
+	os.Exit(1)
+}
+
+func Fatalln(args ...interface{}) {
+	std.println(2, LOG_LEVEL_FATAL, args...)
+	os.Exit(1)
+}
+
+func Fatalf(format string, args ...interface{}) {
+	std.printf(2, LOG_LEVEL_FATAL, format, args...)
+	os.Exit(1)
 }
 
 func Error(args ...interface{}) {
@@ -70,31 +176,73 @@ func Errorf(format string, args ...interface{}) {
 	std.printf(2, LOG_LEVEL_ERROR, format, args...)
 }
 
+func Warn(args ...interface{}) {
+	std.print(2, LOG_LEVEL_WARNING, args...)
+}
+
+func Warnln(args ...interface{}) {
+	std.println(2, LOG_LEVEL_WARNING, args...)
+}
+
+func Warnf(format string, args ...interface{}) {
+	std.printf(2, LOG_LEVEL_WARNING, format, args...)
+}
+
+func Info(args ...interface{}) {
+	std.print(2, LOG_LEVEL_INFO, args...)
+}
+
+func Infoln(args ...interface{}) {
+	std.println(2, LOG_LEVEL_INFO, args...)
+}
+
+func Infof(format string, args ...interface{}) {
+	std.printf(2, LOG_LEVEL_INFO, format, args...)
+}
+
+func Debug(args ...interface{}) {
+	std.print(2, LOG_LEVEL_DEBUG, args...)
+}
+
+func Debugln(args ...interface{}) {
+	std.println(2, LOG_LEVEL_DEBUG, args...)
+}
+
+func Debugf(format string, args ...interface{}) {
+	std.printf(2, LOG_LEVEL_DEBUG, format, args...)
+}
+
 func (l *Logger) SetLogLevel(level int) {
 	l.mu.Lock()
 	defer l.mu.Unlock()
 	l.level = level
 }
 
-func (l *Logger) print(calldepth int, level int, args ...interface{}) {
-	if level <= l.level {
-		l.output(calldepth+1, level, fmt.Sprint(args...))
+func (l *Logger) print(calldepth int, level int, args ...interface{}) string {
+	if level > l.level {
+		return ""
 	}
+	text, _ := l.output(calldepth+1, level, fmt.Sprint(args...))
+	return text
 }
 
-func (l *Logger) println(calldepth int, level int, args ...interface{}) {
+func (l *Logger) println(calldepth int, level int, args ...interface{}) string {
 	if level <= l.level {
-		l.output(calldepth+1, level, fmt.Sprintln(args...))
+		return ""
 	}
+	text, _ := l.output(calldepth+1, level, fmt.Sprintln(args...))
+	return text
 }
 
-func (l *Logger) printf(calldepth int, level int, format string, args ...interface{}) {
+func (l *Logger) printf(calldepth int, level int, format string, args ...interface{}) string {
 	if level <= l.level {
-		l.output(calldepth+1, level, fmt.Sprintf(format, args...))
+		return ""
 	}
+	text, _ := l.output(calldepth+1, level, fmt.Sprintf(format, args...))
+	return text
 }
 
-func (l *Logger) output(calldepth int, level int, s string) error {
+func (l *Logger) output(calldepth int, level int, s string) (string, error) {
 	now := time.Now() // get this early.
 	var file string
 	var line int
@@ -120,7 +268,7 @@ func (l *Logger) output(calldepth int, level int, s string) error {
 
 	l.checkFile(now)
 	_, err := l.out.Write(buf)
-	return err
+	return string(buf), err
 }
 
 func (l *Logger) checkFile(t time.Time) {
@@ -131,10 +279,23 @@ func (l *Logger) checkFile(t time.Time) {
 	if err != nil {
 		return
 	}
-	if l.size >= f.Size() {
-		timestamp = t.Unix()
-		newFileName := fmt.Sprintf("%s.%lld", l.file, timestamp)
-		os.Rename(l.file, newFileName)
+	if int64(l.size) >= f.Size() {
+		switch f := l.out.(type) {
+		case *os.File:
+			if f == os.Stderr {
+
+			} else if f == os.Stdout {
+
+			} else {
+				f.Close()
+				l.out = nil
+				timestamp := t.Unix()
+				newFileName := fmt.Sprintf("%s.%d", l.file, timestamp)
+				os.Rename(l.file, newFileName)
+				f, _ := os.Open(l.file)
+				l.out = f
+			}
+		}
 	}
 }
 
